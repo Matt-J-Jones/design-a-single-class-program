0a6f88d6462ce7df1b3bde36f047668f2afc13a5
Matt Jones <67526111+Matt-J-Jones@users.noreply.github.com>
2023-03-16 19:20:20 +0000

    Prepared files for recorded assessment

--- spec/challenge_spec.rb
+++ spec/challenge_spec.rb
@@ -1,6 +1,5 @@
 # File: spec/challenge_spec.rb
 require 'challenge'
 
-# As a user
-# So that I can keep track of my music listening
-# I want to add tracks I've listened to and see a list of them.
+# I want to add tracks I've listened to 
+# and see a list of them.
