4ccd4104d1fd68756a21b736779ba988321c2815
Matt Jones <67526111+Matt-J-Jones@users.noreply.github.com>
2023-03-17 10:34:52 +0000

    Challenge complete

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -1,1 +1,43 @@
 # File: lib/challenge.rb
+
+class TrackLister 
+  def initialize 
+    @songlist = Array.new
+  end
+
+  def addsong(title, artist)
+    # creates raw list, of items as they are added
+    @songlist << [artist, title]
+  end
+
+  def printlist
+    # pushes raw list to temp array and returns formatted
+    temp_arr = Array.new
+    @songlist.each { |entry| 
+      temp_arr << "#{entry[0]}: #{entry[1]}"
+    }
+    puts temp_arr.join("\n")
+    return temp_arr.join("\n")
+  end
+
+  def printlist_sorted
+    # pushes raw list to temp hash, sorted by artist name,
+    # and returns grouped by artist
+    temp_hash = Hash.new
+    temp_arr = Array.new
+    @songlist.each { |entry| 
+      temp_hash[entry[0]] = []
+    }
+    @songlist.each { |entry|
+      temp_hash[entry[0]] << entry[1]
+    }
+    
+    temp_hash.each { |key, value| 
+      value.each { |song|
+        temp_arr << "#{key}: #{song}"
+      }
+    }
+    puts temp_arr.join("\n")
+    return temp_arr.join("\n")
+  end
+end
--- spec/challenge_spec.rb
+++ spec/challenge_spec.rb
@@ -1,5 +1,31 @@
 # File: spec/challenge_spec.rb
 require 'challenge'
 
-# I want to add tracks I've listened to 
-# and see a list of them.
+
+RSpec.describe TrackLister do
+  # adds one track and returns list
+  it "add's tracks to track listing and returns list" do
+    track_list = TrackLister.new
+    track_list.addsong("Title", "Artist")
+    result = track_list.printlist
+    expect(result).to eq "Artist: Title"
+  end
+  # adds two tracks and returns in played order
+  it "add's tracks to track listing and returns list" do
+    track_list = TrackLister.new
+    track_list.addsong("Title", "Artist")
+    track_list.addsong("Title", "Artist")
+    result = track_list.printlist
+    expect(result).to eq "Artist: Title\nArtist: Title"
+  end
+  # adds multiple tracks and returns grouped by artist
+  it "add's tracks by different artist's and returns grouped by artist" do
+    track_list = TrackLister.new
+    track_list.addsong("Title", "Artist")
+    track_list.addsong("Title", "Artist 2")
+    track_list.addsong("Title 2", "Artist 2")
+    track_list.addsong("Title 2", "Artist")
+    result = track_list.printlist_sorted
+    expect(result).to eq "Artist: Title\nArtist: Title 2\nArtist 2: Title\nArtist 2: Title 2"
+  end
+end
